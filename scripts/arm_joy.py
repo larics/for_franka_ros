a0f63d17e6e40b05a2d20722128afb51c24f5d29
Filip Zoric <fzoric8@gmail.com>
2024-04-24 18:32:21 +0200

    [joy] Some updates

--- scripts/arm_joy.py
+++ scripts/arm_joy.py
@@ -5,6 +5,8 @@
 import rospy
 from geometry_msgs.msg import Twist, Pose, PoseStamped, TwistStamped
 from sensor_msgs.msg import Joy
+from std_msgs.msg import String
+from for_franka_ros.srv import changeState
 
 # TODO: 
 # - [] Add flag for the orientation handling 
@@ -20,9 +22,16 @@ class ArmJoy:
         self.armPoseCurr = Pose() 
         self.enable = False
         self.reciv_pose = False
+        # Ready to send servo cmds to arm
         self.ready = False
+        # Servo flag
         self.servo = True
+        # Change state of the arm to enable servo
+        self.change_state = False
         self.topic_timeout = 1
+        self.state_cnt = 0
+        self.curr_state = "IDLE"
+        self.states = ["JOINT_TRAJ_CTL", "CART_TRAJ_CTL", "SERVO_CTL", "IDLE"]
 
         # Subscriber to joystick topic
         self.joySub = rospy.Subscriber("/joy", Joy, self.joyCallback, queue_size=1)
@@ -32,19 +41,19 @@ class ArmJoy:
             self.armPosePub = rospy.Publisher(f"/{servo_ns}/target_pose", PoseStamped, queue_size=1)
             self.currPoseSub = rospy.Subscriber(f"{servo_ns}/arm/state/current_pose", Pose, self.poseCallback, queue_size=1)
             # TODO: Add changing robot state as a service to the joystick node
-            #self.servoClient = rospy.ServiceClient("")
+            self.stateProxy = rospy.ServiceProxy(f"{servo_ns}/change_state", changeState)
         else: 
             n_servo_ns = "control_arm_node"
             self.armPosePub = rospy.Publisher(f"/{n_servo_ns}/arm/command/cmd_pose", Pose, queue_size=1)
             self.currPoseSub = rospy.Subscriber(f"{n_servo_ns}/arm/state/current_pose", Pose, self.poseCallback, queue_size=1)
 
         # Resolution --> set to be increasable by joystick
-        self.scaleX = 0.05
-        self.scaleY = 0.05
-        self.scaleZ = 0.05
+        self.scaleX = 0.1
+        self.scaleY = 0.1
+        self.scaleZ = 0.01
 
     def run(self):
-        r = rospy.Rate(10)
+        r = rospy.Rate(5)
         while not rospy.is_shutdown():
             if self.reciv_pose: 
                 self.ready = True
@@ -55,21 +64,30 @@ class ArmJoy:
             else: 
                 self.ready = False
 
-            if self.enable and self.ready:
-                armPoseCmd = self.create_arm_cmd()
-                # TODO: Test for the rotation purposes
+            # Recieved relevant pose, and pressed enable
+            if self.ready and self.enable:
+                if self.change_state:
+                    # Change state
+                    resp = self.stateProxy(str(self.states[self.state_cnt]))
+                    self.armPoseCmd = self.create_arm_cmd()
+                    self.change_state = False
+                    self.curr_state = self.states[self.state_cnt] 
+                # Use changed state to publish new joy msg
+                if self.curr_state == "SERVO_CTL":
+                    self.armPoseCmd = self.create_arm_cmd(self.armPoseCmd)
+                    self.armPosePub.publish(self.armPoseCmd)
                 rospy.loginfo_throttle(5.0, "[ArmJoyCtl] On")
-                self.armPosePub.publish(armPoseCmd)
             else:
                 rospy.loginfo_throttle(5.0, "[ArmJoyCtl] Off")
             
             r.sleep()
 
+    
     def create_arm_cmd(self): 
         if self.servo: 
             cmd = PoseStamped()
             cmd.header.stamp = rospy.Time.now()
-            cmd.header.frame_id = "world"
+            cmd.header.frame_id = "panda_hand_tcp"
             # Translation
             cmd.pose.position.x = self.armPoseCurr.position.x + self.dX * self.scaleX
             cmd.pose.position.y = self.armPoseCurr.position.y + self.dY * self.scaleY
@@ -95,7 +113,23 @@ class ArmJoy:
         self.dY = self.joyData.axes[2]
         self.dZ = self.joyData.axes[1]
         self.dYaw = self.joyData.axes[0]
-        self.enable = self.joyData.buttons[7]
+        # Removed from condition
+        if self.joyData.buttons[7] == 1: 
+            self.enable = True
+            rospy.loginfo("Joystick is enabled!")
+        if self.joyData.buttons[5] == 1: 
+            self.enable = False
+            rospy.loginfo("Joystick is disabled!")
+
+        if self.joyData.axes[-1] == 1:
+            if self.state_cnt < 3: 
+                self.state_cnt += 1
+            else: 
+                self.state_cnt = 0 
+            rospy.loginfo(f"Choosen state is: {self.states[self.state_cnt]}")
+
+        if self.joyData.buttons[6] == 1: 
+            self.change_state = True
 
     def poseCallback(self, msg):
         self.reciv_pose = True
